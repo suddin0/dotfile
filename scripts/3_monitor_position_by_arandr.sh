81015b0c5e3a023695791cf8235435bb470e0535
samad <samad@samad.fr>
2020-06-13 13:50:01 +0200

    Multi monitir rotation config for boot up

---
+++ scripts/3_monitor_position_by_arandr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --mode 1680x1050 --pos 2760x1240 --rotate normal --output DisplayPort-1 --primary --mode 1680x1050 --pos 0x1240 --rotate normal --output DisplayPort-2 --mode 2560x1080 --pos 1680x0 --rotate left --output HDMI-A-2 --off --output DVI-D-0 --off --output HDMI1 --off --output HDMI2 --off --output VGA1 --off --output VIRTUAL1 --off
